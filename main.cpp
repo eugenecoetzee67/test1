7e38a64365d16ee23c3ef5f9fde6a56dc13c3b4f
eugenecoetzee67 <eugenecoetzee67@gmail.com>
2015-03-16 16:34:52 +0200

    modify main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 void function1(void)
-{std::cout << "hello world I'm function1" << std::endl; 
+{
+    std::cout << "hello world I'm function1" << std::endl; 
 
 }
 
 int main(int argc, char* argv[])
 {
+    function1();
 
- function1();
-
- return 0;
+    return 0;
 } 
