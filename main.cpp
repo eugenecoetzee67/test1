14f8316e34d412fed6d8e8dfed815ef0c1b400dc
eugenecoetzee67 <eugenecoetzee67@gmail.com>
2015-03-17 13:30:13 +0200

    test thread1 join after detach in  main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,10 @@ int main(int argc, char* argv[])
     //main thread detaches thread1 
     thread1.detach();
     
+    //......
+
+    thread1.join();
+
     //demonstrate the power and syntax of lambda function in C++
     auto myByeFunction = [](std::string paramPrintMessage)->void {   std::cout << "BYE " << paramPrintMessage << std::endl;   };
     myByeFunction(" - this is end of the main thread");
