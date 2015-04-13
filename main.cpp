191e920b349a6ab9769191a5e89055d114bedab4
eugenecoetzee67 <eugenecoetzee67@gmail.com>
2015-04-13 17:54:18 +0200

    make more changes - now ready for testing without mutex protection

--- main.cpp
+++ main.cpp
@@ -9,7 +9,17 @@ std::deque<int> myDeque;
 
 void function1(void)
 {
-    std::cout << "\nhello world I'm function1 in thread1 saying" << std::endl; 
+    std::cout << "\nhello world I'm function in thread1 saying" << std::endl; 
+    int count = 10;
+    while (count > 0)
+    { 
+        myDeque.push_front(count);
+        std::cout << "\ndata in =  " << count << std::endl;
+        count--;
+    }     
+
+
+
 }
 
 class Functor1
@@ -18,35 +28,47 @@ class Functor1
 public:
     Functor1(void)
     {
-        std::cout << "\nFunctor1 constructor" << std::endl; 
+        //std::cout << "\nFunctor1 constructor" << std::endl; 
 
     }
 
     void operator()(std::string paramMessage)
     {
-        std::cout << "hello im functor 1 in thread2 saying" << paramMessage << std::endl;
-     }	
+        std::cout << "hello im functor in thread2 saying" << paramMessage << std::endl;
+        int data = -1;
+        while (data != 1)
+        {
+            if (!myDeque.empty())
+            {
+                data = myDeque.back();
+                std::cout << "\ndata out =  " << data << std::endl;
+                myDeque.pop_back();
+            }
+    
+
+        }
+    }	
 	
 };
 
 int main(int argc, char* argv[])
 {
-    int numCPUs = std::thread::hardware_concurrency();
-    std::cout << "\n number of CPUs = " << numCPUs << std::endl;
-    std::cout << "\n main thread id = " << std::this_thread::get_id() << std::endl;
+    //int numCPUs = std::thread::hardware_concurrency();
+    //std::cout << "\n number of CPUs = " << numCPUs << std::endl;
+    //std::cout << "\n main thread id = " << std::this_thread::get_id() << std::endl;
   
     std::thread thread1(function1);
     Functor1 myFunct1;
     std::thread thread2(myFunct1, "walk like a man");
     
-    std::cout << "\n thread1 id = " << thread1.get_id() << std::endl;
+    //std::cout << "\n thread1 id = " << thread1.get_id() << std::endl;
     
-    std::cout << "\n thread2 id = " << thread2.get_id() << std::endl;
+    //std::cout << "\n thread2 id = " << thread2.get_id() << std::endl;
     
-    for (int i = 0;  i <= 100;  i ++)
-    {
-        std::cout << "MT = " << i << ".";
-    }     
+    //for (int i = 0;  i <= 100;  i ++)
+    //{
+    //    std::cout << "MT = " << i << ".";
+    //}     
 
     if (thread1.joinable() == true)
     {   
@@ -61,8 +83,8 @@ int main(int argc, char* argv[])
     }
 
     //demonstrate the power and syntax of lambda function in C++
-    auto myByeFunction = [](std::string paramPrintMessage)->void {   std::cout << "\nBYE " << paramPrintMessage << std::endl;   };
-    myByeFunction(" - this is end of the main thread");
+    //auto myByeFunction = [](std::string paramPrintMessage)->void {   std::cout << "\nBYE " << paramPrintMessage << std::endl;   };
+    //myByeFunction(" - this is end of the main thread");
     
     std::cout.flush();
     return 0;
