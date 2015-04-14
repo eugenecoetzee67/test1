723f8209e2f8b89a87f1df419f9cc1207c948721
eugenecoetzee67 <eugenecoetzee67@gmail.com>
2015-04-14 13:43:07 +0200

    add mutex and locks to synchronise threads

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <thread>
 #include <mutex>
 #include <deque>
+#include <chrono>
 
 std::mutex mutex1;
 std::deque<int> myDeque; 
-
 
 void function1(void)
 {
@@ -13,8 +13,11 @@ void function1(void)
     int count = 10;
     while (count > 0)
     { 
+        std::unique_lock<std::mutex> lock1(mutex1);
         myDeque.push_front(count);
         std::cout << "\ndata in =  " << count << std::endl;
+        lock1.unlock();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         count--;
     }     
 
@@ -38,11 +41,18 @@ public:
         int data = -1;
         while (data != 1)
         {
+            std::unique_lock<std::mutex> lock1(mutex1);
             if (!myDeque.empty())
             {
                 data = myDeque.back();
                 std::cout << "\ndata out =  " << data << std::endl;
                 myDeque.pop_back();
+                lock1.unlock();
+            }
+            else
+            {
+                lock1.unlock();
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));           
             }
     
 
@@ -57,9 +67,10 @@ int main(int argc, char* argv[])
     //std::cout << "\n number of CPUs = " << numCPUs << std::endl;
     //std::cout << "\n main thread id = " << std::this_thread::get_id() << std::endl;
   
-    std::thread thread1(function1);
+    //std::thread thread1(function1);
     Functor1 myFunct1;
     std::thread thread2(myFunct1, "walk like a man");
+    std::thread thread1(function1);
     
     //std::cout << "\n thread1 id = " << thread1.get_id() << std::endl;
     
