4b5d08a1258eaf9f354832154dd36f970aeb2dfa
eugenecoetzee67 <eugenecoetzee67@gmail.com>
2015-04-20 08:25:50 +0200

    demonstrate the use of promise to pass arguments into a thereaded callable

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@
 
 using namespace std;
 
-int factorial(int N)
+int factorial(future<int>&  paramFutureN)
 {
     int result = 1;
+    cout << "factorial function executing in thread id = " << this_thread::get_id() << endl;
+    cout << "I'm waiting for that promised value of N " << endl;
+    int N = paramFutureN.get();
+    cout << "I got that promised value of N now! " << endl;
     for (int i = N;  i > 1;  i--)
     {
         result *= i; 
     }
     
-    cout << "factorial function executing in thread id = " << this_thread::get_id() << endl;
-    //cout << "result = " << result << endl;
     return result; 
 }
 
@@ -32,11 +34,21 @@ int main(int argc, char* argv[])
     //doesn't compile on g++ 4.6 
     //future<int> future1 = async(launch::deferred, factorial, 4);
    
-    future<int> future1 = async(launch::async, factorial, 4);
-    
-    int result = future1.get();
+    promise<int> promise1;
+    future<int> futureArg = promise1.get_future(); 
+    future<int> future1 = async(launch::async, factorial, ref(futureArg));
+     
+    //sleep a bit 
+    this_thread::sleep_for(chrono::seconds(2));    
 
-    cout << "result = " << result << endl; 
+    promise1.set_value(4);
+
+    //force exception if the promise cannot be kept
+    //promise1.set_exception(make_exception_ptr(runtime_error("to err is human")));
+    
+    int result1 = future1.get();
+
+    cout << "result1 = " << result1 << endl; 
 
     //thread1.join();    
 
