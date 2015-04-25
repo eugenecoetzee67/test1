4e6d1ca059e7ee5b70fcb02afbae29d6c1cba057
eugenecoetzee67 <eugenecoetzee67@gmail.com>
2015-04-25 16:12:23 +0200

    build basic example using file io milestone 1

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: main.cpp
-	g++ -std=c++0x -Wall -g main.cpp -o test1 -lpthread
+	g++ -std=c++0x -Wall -g main2.cpp -o test1 -lpthread
 
 clean:
 	$(RM) test1
--- main2.cpp
+++ main2.cpp
@@ -2,6 +2,8 @@
 #include<fstream>
 #include<vector>
 #include<stdexcept>
+#include<thread>
+#include<chrono>
 
 #include<unistd.h>
 
@@ -18,6 +20,7 @@ string DESTINATION_PATH = "/tmp/error";
 
 vector<char> readInputFile(const string& paramInputFileName)
 {
+    cout << "enter function readInputFile in thread no = " << this_thread::get_id() << endl; 
     if (paramInputFileName.empty())
     {
         throw invalid_argument(EXCEPT_EMPTY_STRING_ARGUMENT);
@@ -28,15 +31,22 @@ vector<char> readInputFile(const string& paramInputFileName)
         cout << "could not open file " << paramInputFileName << endl;
         throw invalid_argument(EXCEPT_ILLEGAL_STRING_ARGUMENT);
     }
- 
-    vector<char> inputBuffer;
+    size_t fileSize = inputFile.tellg(); 
+    cout << "input file size = " << fileSize << " bytes " << endl;
+    vector<char> inputBuffer(fileSize);
+    inputFile.seekg(0, ios::beg);
+    inputFile.read(&inputBuffer[0], fileSize);
+    cout << inputFile.gcount() << " bytes were read " << endl;
     inputFile.close();
+    this_thread::sleep_for(chrono::seconds(5));  
+    cout << "exit function readInputFile in thread no = " << this_thread::get_id() << endl; 
     return inputBuffer;
 
 }
 
 size_t writeOutputFile(const vector<char>& paramInputBuffer, const string& paramOutputFileName)
 {
+    cout << "enter function writeOutputFile in thread no = " << this_thread::get_id() << endl; 
     if (paramOutputFileName.empty())
     {
         throw invalid_argument(EXCEPT_EMPTY_STRING_ARGUMENT);
@@ -45,14 +55,33 @@ size_t writeOutputFile(const vector<char>& paramInputBuffer, const string& param
     {
         throw invalid_argument(EXCEPT_EMPTY_BUFFER_ARGUMENT);
     }
-
-    return 0;
+    ofstream outputFile(paramOutputFileName, ios::binary);
+    if (!outputFile.is_open())
+    {
+        cout << "could not open file " << paramOutputFileName << endl;
+        throw invalid_argument(EXCEPT_ILLEGAL_STRING_ARGUMENT);
+    }
+    outputFile.write(&paramInputBuffer[0], paramInputBuffer.size());
+    size_t numberOfBytesWritten = outputFile.tellp();
+    outputFile.close();
+    cout << numberOfBytesWritten << " bytes were written " << endl;
+    cout << "exit function writeOutputFile in thread no = " << this_thread::get_id() << endl; 
+    return numberOfBytesWritten;
 }     
+
+size_t syncedReadWriteFile(const string& paramInputFileName, const string& paramOutputFileName)
+{
+   cout << "enter function syncedReadWriteFile in thread no = " << this_thread::get_id() << endl; 
+   size_t numberOfBytesWritten = writeOutputFile(readInputFile(paramInputFileName), paramOutputFileName);
+   cout << "exit function syncedReadWriteFile in thread no = " << this_thread::get_id() << endl; 
+   return numberOfBytesWritten;
+}
 
 
 
 int main(int argc, char* argv[])
-{
+{   cout << "\x1B[2J\x1B[H"; 
+    cout << "main function in thread no = " << this_thread::get_id() << endl; 
     string sourceFileName = "/tmp/error/error.txt";
     string destinationFileName = "/tmp/error/error.txt";
     char* currentPath = get_current_dir_name();
@@ -67,9 +96,14 @@ int main(int argc, char* argv[])
     cout << "source filename = " << sourceFileName << endl;
     cout << "destination filename = " << destinationFileName << endl;
     
-    vector<char> inputBuffer = readInputFile(sourceFileName); 
-    
+    int numberOfBytesWritten = syncedReadWriteFile(sourceFileName,destinationFileName);    
 
+    //let main trad do something until ???
+    for (int i = 0; i <= 500000; i++)
+    {    
+        cout << i;
+    }
+    cout << "\nbye" << endl;
     return EXIT_SUCCESS;
 
 }
